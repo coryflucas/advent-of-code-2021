a712daa472c50b7607f20fee51b42f5bf5d61240
Cory Lucas <cory@corylucas.com>
2021-12-04 21:55:26 -0500

    day2

---
+++ day2-part2/Program.cs
@@ -0,0 +1,24 @@
+﻿var position = File.ReadLines("input.txt")
+    .Select(line => line.Split(' '))
+    .Select(lineParts => new
+    {
+        Direction = lineParts[0],
+        Amount = int.Parse(lineParts[1])
+    })
+    .Aggregate(
+        new Position(0, 0, 0),
+        (position, instruction) => instruction.Direction switch
+        {
+            "forward" => Forward(position, instruction.Amount),
+            "down" => Down(position, instruction.Amount),
+            "up" => Up(position, instruction.Amount),
+            _ => throw new Exception($"Unexpected direction {instruction.Direction}")
+        }
+    );
+
+Console.WriteLine(position.Horizontal * position.Depth);
+
+Position Forward(Position current, int amount) => new Position(current.Horizontal + amount, current.Depth + (current.Aim * amount), current.Aim);
+Position Up(Position current, int amount) => new Position(current.Horizontal, current.Depth, current.Aim - amount);
+Position Down(Position current, int amount) => new Position(current.Horizontal, current.Depth, current.Aim + amount);
+record Position(int Horizontal, int Depth, int Aim);
