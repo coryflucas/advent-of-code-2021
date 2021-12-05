78cfde752487eb0d8c2fb7b4e6cb44150cf0ae36
Cory Lucas <cory@corylucas.com>
2021-12-04 22:58:24 -0500

    add day 3 and solution

---
+++ day3-part1/Program.cs
@@ -0,0 +1,20 @@
+﻿var numberOfBits = File.ReadLines("input.txt")
+    .First()
+    .Count();
+
+var inputs = File.ReadLines("input.txt")
+    .Select(line => Convert.ToInt32(line, 2))
+    .ToList();
+
+var gamma = 0;
+var epsilon = 0;
+for (int i = 0; i < numberOfBits; i++)
+{
+    var numberOfOnes = inputs.Select(input => (input >> i) & 1)
+        .Sum();
+    var mostCommonBit = numberOfOnes > (inputs.Count / 2) ? 1 : 0;
+    gamma += mostCommonBit << i;
+    epsilon += (mostCommonBit ^ 1) << i;
+}
+
+Console.WriteLine(gamma * epsilon);
---
+++ day3-part2/Program.cs
@@ -0,0 +1,38 @@
+﻿var numberOfBits = File.ReadLines("input.txt")
+    .First()
+    .Count();
+
+var inputs = File.ReadLines("input.txt")
+    .Select(line => Convert.ToInt32(line, 2))
+    .ToList();
+
+var oxygenRatingPosibilities = inputs;
+for (int i = numberOfBits - 1; i >= 0; i--)
+{
+    var numberOfOnes = oxygenRatingPosibilities.Select(input => (input >> i) & 1)
+        .Sum();
+    var mostCommonBit = numberOfOnes >= (oxygenRatingPosibilities.Count / 2.0) ? 1 : 0;
+    oxygenRatingPosibilities = oxygenRatingPosibilities.Where(input => ((input >> i) & 1) == mostCommonBit)
+        .ToList();
+    if (oxygenRatingPosibilities.Count == 1)
+    {
+        break;
+    }
+}
+
+var co2RatingPosibilities = inputs;
+for (int i = numberOfBits - 1; i >= 0; i--)
+{
+    var numberOfOnes = co2RatingPosibilities.Select(input => (input >> i) & 1)
+        .Sum();
+    var mostCommonBit = numberOfOnes >= (co2RatingPosibilities.Count / 2.0) ? 0 : 1;
+
+    co2RatingPosibilities = co2RatingPosibilities.Where(input => ((input >> i) & 1) == mostCommonBit)
+        .ToList();
+    if (co2RatingPosibilities.Count == 1)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(oxygenRatingPosibilities.Single() * co2RatingPosibilities.Single());
