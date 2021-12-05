73742a3e03706456bf1afc3f8d13bcbcd0194790
Cory Lucas <cory@corylucas.com>
2021-12-04 21:32:13 -0500

    day1

---
+++ day1-part1/Program.cs
@@ -0,0 +1,14 @@
+﻿var input = File.ReadLines("input.txt")
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+var increaseingCount = 0;
+for (var i = 1; i < input.Count(); i++)
+{
+    if (input[i] > input[i - 1])
+    {
+        increaseingCount++;
+    }
+}
+
+Console.WriteLine(increaseingCount);
---
+++ day1-part2/Program.cs
@@ -0,0 +1,19 @@
+﻿var input = File.ReadLines("input.txt")
+    .Select(x => int.Parse(x))
+    .ToArray();
+
+var windows = input
+    .Skip(2)
+    .Select((x, i) => x + input[i] + input[i + 1])
+    .ToArray();
+
+var increaseingCount = 0;
+for (var i = 1; i < windows.Count(); i++)
+{
+    if (windows[i] > windows[i - 1])
+    {
+        increaseingCount++;
+    }
+}
+
+Console.WriteLine(increaseingCount);
